4041d30ae4b62652a780c41378b18742a1b95bed
lameesad <lameesaboudarwish@gmail.com>
2024-01-19 17:54:04 +0400

    error handling for github apis

--- app/lib/strapi/github.ts
+++ app/lib/strapi/github.ts
@@ -1,80 +1,112 @@
-// lib/github.ts
-
-import { Repository, UserProfile } from '../definitions';
-
-export const fetchGitHubUserProfile = async (
-  username: string,
-): Promise<UserProfile | null> => {
-  const response = await fetch(`https://api.github.com/users/${username}`);
-  if (!response.ok) {
-    // throw new Error(`Failed to fetch GitHub user. Status: ${response.status}`);
-    return null;
+export const fetchGitHubUserProfile = async (username: string) => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    if (!response.ok) {
+      return {
+        data: null,
+        error: `Failed to fetch GitHub user. Status: ${response.status}`,
+      };
+    }
+    const data = await response.json();
+    return {
+      data: {
+        username: data.login,
+        email: data.email,
+        image: data.avatar_url,
+        followers: data.followers,
+        repository: data.public_repos,
+        stars: 0, // Will be calculated separately
+        readme: null, // Will be fetched separately
+      },
+      error: null,
+    };
+  } catch (error) {
+    return {
+      data: null,
+      error: `Error fetching GitHub user profile: ${error}`,
+    };
   }
-  const data = await response.json();
-  return {
-    username: data.login,
-    email: data.email, // Note: GitHub API may not return email
-    image: data.avatar_url,
-    followers: data.followers,
-    repository: data.public_repos,
-    stars: 0, // Will be calculated separately
-    readme: null, // Will be fetched separately
-  };
 };
 
-export const fetchGitHubUserRepos = async (
-  username: string,
-): Promise<Repository[] | null> => {
-  const response = await fetch(
-    `https://api.github.com/users/${username}/repos`,
-  );
-  if (!response.ok) {
-    // throw new Error(`Failed to fetch GitHub repos. Status: ${response.status}`);
-    return null;
+export const fetchGitHubUserRepos = async (username: string) => {
+  try {
+    const response = await fetch(
+      `https://api.github.com/users/${username}/repos`,
+    );
+    if (!response.ok) {
+      return {
+        data: null,
+        error: `Failed to fetch GitHub repos. Status: ${response.status}`,
+      };
+    }
+    const repos = await response.json();
+    return { data: repos, error: null };
+  } catch (error) {
+    return { data: null, error: `Error fetching GitHub user repos: ${error}` };
   }
-  return response.json();
 };
 
 export const fetchGitHubRepoReadme = async (
   username: string,
   repoName: string,
-): Promise<string> => {
-  const response = await fetch(
-    `https://api.github.com/repos/${username}/${repoName}/readme`,
-    {
-      headers: { Accept: 'application/vnd.github.VERSION.raw' },
-    },
-  );
-  if (!response.ok) {
-    // throw new Error(`Failed to fetch README. Status: ${response.status}`);
-    return '';
+) => {
+  try {
+    const response = await fetch(
+      `https://api.github.com/repos/${username}/${repoName}/readme`,
+      { headers: { Accept: 'application/vnd.github.VERSION.raw' } },
+    );
+    if (!response.ok) {
+      return {
+        data: '',
+        error: `Failed to fetch README. Status: ${response.status}`,
+      };
+    }
+    const readmeContent = await response.text();
+    return { data: readmeContent, error: null };
+  } catch (error) {
+    return { data: '', error: `Error fetching GitHub repo README: ${error}` };
   }
-  return response.text();
 };
 
-export async function fetchGitHubData(
-  username: string,
-): Promise<UserProfile | null> {
+export async function fetchGitHubData(username: string) {
   if (!username) {
-    return null;
+    return { data: null, error: 'Username is required' };
   }
-  const userProfile = await fetchGitHubUserProfile(username);
-  const userRepos = await fetchGitHubUserRepos(username);
-  if (!userProfile || !userRepos) {
-    return null;
+
+  const userProfileResponse = await fetchGitHubUserProfile(username);
+  if (userProfileResponse.error) {
+    return { data: null, error: userProfileResponse.error };
   }
-  const starsCount = userRepos.reduce(
-    (acc, repo) => acc + repo.stargazers_count,
+
+  const userReposResponse = await fetchGitHubUserRepos(username);
+  if (userReposResponse.error) {
+    return { data: null, error: userReposResponse.error };
+  }
+
+  const starsCount = userReposResponse.data.reduce(
+    (acc: any, repo: { stargazers_count: any }) => acc + repo.stargazers_count,
     0,
   );
+
   let readmeContent = '';
-  if (userRepos.length > 0) {
-    readmeContent = await fetchGitHubRepoReadme(username, userRepos[0].name);
+  if (userReposResponse.data.length > 0) {
+    const readmeResponse = await fetchGitHubRepoReadme(
+      username,
+      userReposResponse.data[0].name,
+    );
+    if (readmeResponse.error) {
+      readmeContent = readmeResponse.error;
+    } else {
+      readmeContent = readmeResponse.data;
+    }
   }
-  console.log(userProfile);
+
   return {
-    ...userProfile,
-    stars: starsCount,
-    readme: readmeContent,
+    data: {
+      ...userProfileResponse.data,
+      stars: starsCount,
+      readme: readmeContent,
+    },
+    error: null,
   };
 }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,26 +1,17 @@
 'use client';
-import React, { use, useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { UserProfile } from '../lib/definitions';
 import { fetchGitHubData } from '../lib/strapi/github';
 import { getUser, updateUser } from '../lib/strapi/data';
 import Image from 'next/image';
 import { ArrowPathIcon } from '@heroicons/react/20/solid';
 import { Button } from '@/components/ui/button';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Card } from '@/components/ui/card';
 import UserContext from '../lib/context/User';
 import { useRouter } from 'next/navigation';
+import { set } from 'zod';
 
 const ProfilePage: React.FC = () => {
-  const defaultUser: UserProfile = {
-    username: 'Default User',
-    email: 'default@example.com',
-    image: 'https://avatars.githubusercontent.com/exampleuser',
-    followers: 0,
-    repository: 0,
-    stars: 0,
-    readme: 'Default README content',
-  };
   const [user, setUser] = useState<UserProfile | null>();
   const [loading, setLoading] = useState(true);
   const router = useRouter();
@@ -39,15 +30,16 @@ const ProfilePage: React.FC = () => {
     console.log('Loading profile...');
     try {
       const userData = await getUser();
-      console.log(userData);
-      const githubData = await fetchGitHubData(userData.username);
-      console.log(githubData);
-      setUser(githubData ? githubData : userData);
-      console.log(githubData);
+      const { data: githubData } = await fetchGitHubData(userData.username);
+      if (!githubData) {
+        setLoading(false);
+        setUser(userData);
+        return;
+      }
+      setUser(githubData as UserProfile);
     } catch (error) {
       console.error('Error:', error);
       setLoading(false);
-      // setUser(defaultUser);
     } finally {
       setLoading(false);
     }
@@ -58,9 +50,10 @@ const ProfilePage: React.FC = () => {
     try {
       const userData = await getUser();
       console.log(userData);
-      const githubData = await fetchGitHubData(userData.username);
-      setUser(githubData);
-      await updateUser(githubData); // Update user data in DB
+      const { data, error } = await fetchGitHubData(userData.username);
+      if (error) return;
+      setUser(data as UserProfile);
+      await updateUser(data); // Update user data in DB
     } catch (error) {
       console.error('Error:', error);
     } finally {
@@ -73,60 +66,56 @@ const ProfilePage: React.FC = () => {
   }
 
   return (
-    <>
-      <div className="flex h-full justify-center bg-gray-100">
-        {user && (
-          <Card className="my-10 w-full max-w-4xl rounded-lg bg-white p-6 shadow-lg">
-            <div className="flex items-center justify-between border-b border-gray-300 pb-4">
-              <div className="flex items-center">
-                <Image
-                  src={
-                    user.image ||
-                    'https://avatars.githubusercontent.com/exampleuser'
-                  }
-                  alt="Profile Picture"
-                  width={100}
-                  height={100}
-                  className="rounded-full"
-                />
-                <div className="ml-4">
-                  <h1 className="text-2xl font-bold">{user.username}</h1>
-                  <p className="text-sm text-gray-600">{user.email}</p>
-                </div>
+    <div className="flex h-full justify-center bg-gray-100">
+      {user && (
+        <Card className="my-10 w-full max-w-4xl rounded-lg bg-white p-6 shadow-lg">
+          <div className="flex items-center justify-between border-b border-gray-300 pb-4">
+            <div className="flex items-center">
+              <Image
+                src={
+                  user.image ||
+                  'https://avatars.githubusercontent.com/exampleuser'
+                }
+                alt="Profile Picture"
+                width={100}
+                height={100}
+                className="rounded-full"
+              />
+              <div className="ml-4">
+                <h1 className="text-2xl font-bold">{user.username}</h1>
+                <p className="text-sm text-gray-600">{user.email}</p>
               </div>
-              <Button
-                onClick={refreshProfile}
-                className="flex items-center rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
-              >
-                <ArrowPathIcon className="mr-2 h-5 w-5" aria-hidden="true" />
-                Profile
-              </Button>
             </div>
+            <Button
+              onClick={refreshProfile}
+              className="flex items-center rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+            >
+              <ArrowPathIcon className="mr-2 h-5 w-5" aria-hidden="true" />
+              Profile
+            </Button>
+          </div>
 
-            <div className="mt-4 grid grid-cols-3 gap-4 text-center">
-              <div>
-                <p className="text-gray-700">{user.followers}</p>
-                <h3 className="text-lg font-semibold">Followers</h3>
-              </div>
-              <div>
-                <p className="text-gray-700">{user.repository}</p>
-                <h3 className="text-lg font-semibold">Repositories</h3>
-              </div>
-              <div>
-                <p className="text-gray-700">{user.stars}</p>
-                <h3 className="text-lg font-semibold">Stars</h3>
-              </div>
+          <div className="mt-4 grid grid-cols-3 gap-4 text-center">
+            <div>
+              <p className="text-gray-700">{user.followers}</p>
+              <h3 className="text-lg font-semibold">Followers</h3>
             </div>
-            <div className="mt-5">
-              <h2 className="text-lg font-semibold">Bio</h2>
-              <p className="text-gray-700">
-                {user.readme || 'No bio available'}
-              </p>
+            <div>
+              <p className="text-gray-700">{user.repository}</p>
+              <h3 className="text-lg font-semibold">Repositories</h3>
             </div>
-          </Card>
-        )}
-      </div>
-    </>
+            <div>
+              <p className="text-gray-700">{user.stars}</p>
+              <h3 className="text-lg font-semibold">Stars</h3>
+            </div>
+          </div>
+          <div className="mt-5">
+            <h2 className="text-lg font-semibold">Bio</h2>
+            <p className="text-gray-700">{user.readme ?? 'No bio available'}</p>
+          </div>
+        </Card>
+      )}
+    </div>
   );
 };
 
